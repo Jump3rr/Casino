c12c5cbb8c7f0566483ee90c2b973b3b0bf8bee6
Patryk Kamusiński <theneonxd@gmail.com>
2022-12-21 16:40:04 +0100

    login page

--- src/Login/LoginPage.tsx
+++ src/Login/LoginPage.tsx
@@ -3,6 +3,66 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../tools/firebaseConfig';
 import styled from 'styled-components';
+import { Colors } from '../entities/colors';
+
+const LoginWrapper = styled.div`
+  background-color: ${Colors.mainGreen};
+  height: 80vh;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+const Input = styled.input`
+  height: 2em;
+  width: 30em;
+  padding: 0.5em;
+  margin: 0.5em;
+  background-color: ${Colors.lighterGreen};
+  color: ${Colors.gold};
+  border-color: ${Colors.mainGreen};
+  border-width: 1px;
+  border-radius: 10px;
+  font-size: large;
+  :focus {
+    border: 1px solid ${Colors.gold};
+    outline: none;
+    //border-color: ${Colors.red};
+  }
+
+  ::placeholder {
+    color: ${Colors.gold};
+    opacity: 0.5;
+  }
+`;
+const LoginButton = styled.button`
+  margin: 1em;
+  background-color: ${Colors.gold};
+  height: 2.5em;
+  width: 31em;
+  font-size: large;
+  font-weight: bold;
+  color: ${Colors.lighterGreen};
+  cursor: pointer;
+  border-radius: 10px;
+
+  :hover {
+    background-color: ${Colors.red};
+    color: ${Colors.gold};
+  }
+`;
+const Buttons = styled.div`
+  font-size: larger;
+  font-weight: bold;
+  cursor: pointer;
+  margin: 0.5em;
+
+  :hover {
+    text-decoration: underline;
+    color: ${Colors.red};
+  }
+`;
 
 export const LoginPage = () => {
   const [email, setEmail] = useState('');
@@ -19,20 +79,26 @@ export const LoginPage = () => {
     }
   };
   return (
-    <>
-      <input
+    <LoginWrapper>
+      <h1>LOGO</h1>
+      <h3>Welcome to the Grand Luck Casino</h3>
+      <h4>Please, login to play</h4>
+      <Input
         type='text'
         placeholder='Email'
         onChange={(event) => setEmail(event.target.value)}
       />
-      <input
+      <Input
         type='text'
         placeholder='Password'
         onChange={(event) => setPassword(event.target.value)}
       />
-      <button onClick={login}>Login</button>
-      <div>logged as : {auth.currentUser?.email}</div>
-    </>
+      <LoginButton onClick={login}>Login</LoginButton>
+      <Buttons onClick={() => navigate('/register')}>
+        Create new account
+      </Buttons>
+      <Buttons onClick={() => navigate('/')}>Forgot password?</Buttons>
+    </LoginWrapper>
   );
 };
 
---
+++ src/entities/colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+  mainGreen: '#0f2612',
+  lighterGreen: '#2c6133',
+  black: '#000',
+  white: '#fff',
+  darkRed: '#59101c',
+  gold: '#FFD700',
+  red: '#cc2929',
+};
